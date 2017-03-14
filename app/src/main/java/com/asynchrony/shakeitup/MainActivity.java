5097e4bdb5d99f9f830ad02f240b4a1552fbe4ab
EmmettWilson <Mathematicalfunk@gmail.com>
2017-03-14 05:36:50 -0500

    - Turn sensor values into a stream. Observe the xValue, and log the result. ()

--- app/src/main/java/com/asynchrony/shakeitup/MainActivity.java
+++ app/src/main/java/com/asynchrony/shakeitup/MainActivity.java
@@ -1,13 +1,44 @@
 package com.asynchrony.shakeitup;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function3;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.BehaviorSubject;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    public static final String TAG = MainActivity.class.getSimpleName();
     private AudioPlayer audioPlayer;
+    private final BehaviorSubject<Float> xValue = BehaviorSubject.create();
+    private final BehaviorSubject<Float> yValue = BehaviorSubject.create();
+    private final BehaviorSubject<Float> zValue = BehaviorSubject.create();
+
+    private SensorEventListener sensorEventListener;
+    private SensorManager sensorManager;
+    private Disposable disposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +54,43 @@ public class MainActivity extends AppCompatActivity {
                 audioPlayer.play();
             }
         });
+    }
 
-        findViewById(R.id.pause_button).setOnClickListener(new View.OnClickListener() {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        final Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(sensorEventListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorEventListener = new SensorEventListener() {
             @Override
-            public void onClick(View v) {
-                audioPlayer.pause();
+            public void onSensorChanged(final SensorEvent event) {
+                xValue.onNext(event.values[0]);
+                xValue.onNext(event.values[1]);
+                xValue.onNext(event.values[2]);
+            }
+
+            @Override
+            public void onAccuracyChanged(final Sensor sensor, final int accuracy) {
+                //No op
+            }
+        };
+
+        Disposable disposable = xValue.subscribe(new Consumer<Float>() {
+            @Override
+            public void accept(Float aFloat) throws Exception {
+                Log.i(TAG, "Received xValue " + aFloat);
             }
         });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        sensorManager.unregisterListener(sensorEventListener);
+        if(disposable != null && !disposable.isDisposed()){
+            disposable.dispose();
+        }
     }
 
     @Override
